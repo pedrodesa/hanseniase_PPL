ba11f2003eb6cb350c15d631cf6991d8a9980f98
Pedro <pedrodesa42@gmail.com>
2022-03-09 10:55:10 -0300

    versão 1

---
+++ SCRIPT00_instal_pacot.R
@@ -0,0 +1,31 @@
+########################
+# Instalação de pacotes
+pacotes <- c('data.table', 
+             'dplyr',
+             'formattable',
+             'tidyr',
+             'tidyverse',
+             'ggplot2',
+             'descr',
+             'dplyr',
+             'lubridate',
+             'markdown',
+             'formattable',
+             'knitr',
+             'scales',
+             'ggpubr',
+             'foreign'
+             )
+
+if(sum(as.numeric(!pacotes %in% installed.packages())) != 0){
+  instalador <- pacotes[!pacotes %in% installed.packages()]
+  for(i in 1:length(instalador)) {
+    install.packages(instalador, dependencies = T)
+    break()}
+  sapply(pacotes, require, character = T) 
+} else {
+  sapply(pacotes, require, character = T) 
+}
+
+
+theme_set(theme_pubr())
